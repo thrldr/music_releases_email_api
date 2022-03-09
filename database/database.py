3c59722f3e8dfbb1210d2ed8a8025ff413fc1477
thrldr <thrldr@mail.ru>
2022-03-10 00:37:52 +0300

    Connected to db.

--- database/database.py
+++ database/database.py
@@ -1,2 +1,12 @@
 import psycopg2
 from psycopg2 import sql
+
+conn = psycopg2.connect(database="mydb", user="thrldr", password="gibson777",
+        host="localhost", port=5432)
+
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM persons")
+data = cur.fetchall()
+print(data)
+
